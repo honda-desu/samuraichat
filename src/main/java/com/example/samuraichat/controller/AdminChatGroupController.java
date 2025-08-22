421b4c677d344dd8acfaab9e9cab3fea135b45b8
honda-desu <h9.k.honda@gmail.com>
2025-08-22 11:27:32 +0900

    add feature pagenation

--- src/main/java/com/example/samuraichat/controller/AdminChatGroupController.java
+++ src/main/java/com/example/samuraichat/controller/AdminChatGroupController.java
@@ -2,6 +2,8 @@ package com.example.samuraichat.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +22,7 @@ public class AdminChatGroupController {
 	}
 	
 	@GetMapping
-	public String index(Pageable pageable, Model model) {
+	public String index(@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, Model model) {
 		Page<ChatGroup> chatGroupPage = chatGroupService.findAllChatGroups(pageable);
 		
 		model.addAttribute("chatGroupPage", chatGroupPage);
