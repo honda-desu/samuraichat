9eebf7362eee31c047d28daae972cd42f071721f
honda-desu <h9.k.honda@gmail.com>
2026-03-23 11:04:08 +0900

    fix encoding for chat.js

--- src/main/resources/static/js/Chat.js
+++ src/main/resources/static/js/Chat.js
@@ -2,14 +2,14 @@ function openProfileModal(el) {
     const name = el.getAttribute("data-name");
     const text = el.getAttribute("data-text");
     const image = el.getAttribute("data-image");
-    const userId = el.getAttribute("data-user-id"); // ★ 追加
+    const userId = el.getAttribute("data-user-id");
 
 
     document.getElementById("modalUserName").textContent = name;
     document.getElementById("modalUserText").textContent = text;
     document.getElementById("modalProfileImage").src = image;
     
-    // ★ DMボタンのリンクを動的に設定
+    
     const dmButton = document.getElementById("modalDmButton");
     dmButton.href = "/dm/room/" + userId;
 
